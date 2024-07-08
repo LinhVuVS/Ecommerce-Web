8ce23b522ad6389d6af05856905ca48682a300ed
LinhVuVS <167048511+LinhVuVS@users.noreply.github.com>
2024-07-08 16:38:55 +0700

    Auth&Layout

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+import LeftSideBar from "@/components/Layout/LeftSideBar";
+import TopBar from "@/components/Layout/TopSideBar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Borcelle - Admin Dashboard",
+  description: "Admin dashboard to manage Borcelle's data",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <header>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+          </header>
+          <div className="flex max-lg:flex-col text-grey-1">
+            <LeftSideBar />
+            <TopBar />
+            <div className="flex-1">{children}</div>
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
